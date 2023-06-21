405440c081c70d0da6b1b3d74d5cf0bf68fa3300
The Brahmnic Boy <thebrahmnicboy@gmail.com>
2023-06-21 22:55:09 +0530

    feat: add forms and lobby layout

--- scripts/lobby.js
+++ scripts/lobby.js
@@ -1,3 +1,15 @@
 import { setInitialTheme } from "./theme.js";
+import { isPrivateChkBox, passwordInput } from "./lobby/rooms.js";
 
 setInitialTheme();
+
+isPrivateChkBox.addEventListener("change", (event) => {
+    const checked = event.target.checked;
+    if (checked) {
+        passwordInput.classList.remove("hidden");
+        passwordInput.disabled = false;
+    } else {
+        passwordInput.classList.add("hidden");
+        passwordInput.disabled = true;
+    }
+});
---
+++ scripts/lobby/rooms.js
@@ -0,0 +1,2 @@
+export const isPrivateChkBox = document.getElementById("private");
+export const passwordInput = document.getElementById("create-room-password");
